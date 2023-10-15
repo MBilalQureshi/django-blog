9d2e97c921a0d7b7a2311d0fee22e4fd731f35d1
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-10-15 21:31:54 +0000

    Registed commnets table, customize it and add actions for approved comments

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Register your models here.
 # Now tell admin our text fileds are going to be summer note fields
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
@@ -15,5 +16,16 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
 
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    # actions will allow admin to perform differnt actions, bt
+    # default there is only delete, REMEMBER Comments table have
+    # approved that is bydefault false set by us
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 # admin.site.register(Post)

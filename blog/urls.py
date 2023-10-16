de73327722dbf0576923bbf55df088ef8e12e891
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-10-16 18:22:16 +0000

    Add view new posts functionality and install django all auth for autherisation

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ from django.urls import path, include
 # end of PostList, so that it render this class as view
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    # first slug is path converter, second slug is keywordname which can be anything
+    # https://docs.djangoproject.com/en/3.2/topics/http/urls/#how-django-processes-a-request
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
 ]
